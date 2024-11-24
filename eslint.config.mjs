cd3492be0fbdc9f4fe1107d42c5106bd0da58edc
David Rivera <driveran2210@gmail.com>
2024-11-23 21:47:27 -0600

    Add(PK-02): precommit standards

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      semi: ['warn', 'always'],
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
